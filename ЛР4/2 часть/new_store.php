1639364b81ad3e564c68eeb133be453c7194fcec
Filatov3107 <80103704+Filatov3107@users.noreply.github.com>
2021-12-24 20:26:11 +0300

    Add files via upload

---
+++ ЛР4/2 часть/new_store.php
@@ -0,0 +1,14 @@
+<html>
+<head><title> Добавление нового магазина </title></head>
+<body>
+<H2>Добавление нового магазина:</H2>
+<form action="save_new_store.php" method="get">
+    Название: <input name="name_as" size="50" type="text">
+    <br>Адрес: <input name="address" size="50" type="text">
+    <p><input name="add" type="submit" value="Добавить">
+        <input name="b2" type="reset" value="Очистить"></p>
+</form>
+<p>
+    <a href="stores.php"> Вернуться к списку магазинов </a>
+</body>
+</html>
